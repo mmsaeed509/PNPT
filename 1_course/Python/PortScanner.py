fd32a7c86b4fc8f298e8996b2c0de41661ec8a65
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-12-16 00:44:07 +0200

    updated src code

--- 1_course/Python/PortScanner.py
+++ 1_course/Python/PortScanner.py
@@ -1,34 +1,32 @@
 #!/bin/python3
 
-####################################################################################################################
-
 import sys
 import socket
 from datetime import datetime
 
 # RUN script with IP as argument #
 
-if len(sys.argv) == 2 :
-    IP = socket.gethostbyname(sys.argv[1]) # translates hostname to IPv4 #
+if len(sys.argv) == 2:
+    IP = socket.gethostbyname(sys.argv[1])  # translates hostname to IPv4 #
 else:
     print("Invalid number of arguments \nRUN `python3 PortScanner.py <IP>`")
 
 # add a pretty panner #
 print("━" * 50)
 print("Scanning Target : " + IP)
-print("Time starte : " + str(datetime.now()))
+print("Time start : " + str(datetime.now()))
 print("━" * 50)
 
 try:
- 
-    for port in range(1,65535):
+
+    for port in range(1, 65535):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(0.5)
         result = s.connect_ex((IP, port))
         # print("Checking port {} ... ".format(port))
 
         if result == 0:
-            print("Port {} is Opend!".format(port))
+            print("Port {} is Opened!".format(port))
         s.close()
 
 except  KeyboardInterrupt:
